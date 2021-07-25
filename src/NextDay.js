7eb291aeab97b9f8b64731a294443b12a5f224ea
Barbara Oleszek <barbaraoleszek@gmail.com>
2021-07-25 22:09:00 +0200

    fixed icons

--- src/NextDay.js
+++ src/NextDay.js
@@ -2,14 +2,17 @@ import React from "react";
 
 
 export default function NextDay(props) {
+console.log(props.data);
+
   return props.data.map(function(dailyData, index) {
     if (index < 5) {
+      let forecastIcon = `http://openweathermap.org/img/wn/${dailyData.weather[0].icon}@2x.png`; 
       return (
         <div className="weather-forecast">
           <div className="row p-2 justify-content-center first" key={index}>
             <div className="col p-2 col-sm-2 day">{new Date(dailyData.dt *1000).toLocaleString("default", { weekday: "long" })}</div>
             <div className="col p-2 col-sm-2 icon">
-              <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="icon" />
+              <img src={forecastIcon} alt="icon" />
             </div>
             <div className="col p-2 col-sm-2 temperature">{Math.round(dailyData.temp.max)}°/{Math.round(dailyData.temp.min)}°</div>
           </div>
